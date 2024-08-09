37ef3c65a911bcacf7b990f9aa5a847f8bdeb0c7
Maley Venkatesh <127150040+Venkatesh-Maley@users.noreply.github.com>
2024-08-09 19:07:08 +0530

    Add files via upload

---
+++ AngularFrontEnd/src/app/interceptors/my-interceptor.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class MyInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const modifiedReq = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer your-token')
+    });
+
+    return next.handle(modifiedReq);
+  }
+}
